8e490408e8029723e43c5c096ddd3b782c03eae5
ArunArunAk <arunarun2gs@gmail.com>
2025-12-31 07:18:45 +0530

    Add mongo url new with space in ci

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 // Replace with your MongoDB connection string
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/studentdb";
 
